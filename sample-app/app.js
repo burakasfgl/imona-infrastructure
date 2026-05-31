6ca9cf5d9f81a747d32852f672f985195ce58a6c
Burak Sofuoğlu <buraksm202@gmail.com>
2026-06-01 01:35:23 +0300

    gamification api

--- sample-app/app.js
+++ sample-app/app.js
@@ -1,40 +1,100 @@
-const http = require("http");
+const express = require("express");
 const { MongoClient } = require("mongodb");
+
+const app = express();
+
+app.use(express.json());
 
 const uri = process.env.MONGO_URI;
 
-async function connectMongo() {
+let db;
 
-  try {
+async function connectMongo(){
 
     const client = new MongoClient(uri);
 
     await client.connect();
 
+    db = client.db("imona");
+
     console.log("Mongo Connected");
-
-  } catch (err) {
-
-    console.error(err);
-
-  }
 
 }
 
 connectMongo();
 
-const server = http.createServer((req,res)=>{
+app.get("/",(req,res)=>{
 
-  res.writeHead(200, {
-    "Content-Type":"text/plain; charset=utf-8"
-  });
-
-  res.end("Mongo Connected 🚀");
+    res.send("Imona Gamification Running 🚀");
 
 });
 
-server.listen(3000, ()=>{
 
-  console.log("Server running on port 3000");
+app.post("/xp", async (req,res)=>{
+
+    const { user, points } = req.body;
+
+    await db.collection("xp").insertOne({
+
+        user,
+
+        points,
+
+        createdAt:new Date()
+
+    });
+
+    res.json({
+
+        message:"xp added"
+
+    });
+
+});
+
+
+app.get("/leaderboard", async (req,res)=>{
+
+    const leaderboard = await db
+      .collection("xp")
+      .aggregate([
+
+        {
+
+          $group:{
+
+            _id:"$user",
+
+            totalXP:{
+
+              $sum:"$points"
+
+            }
+
+          }
+
+        },
+
+        {
+
+          $sort:{
+
+            totalXP:-1
+
+          }
+
+        }
+
+      ])
+      .toArray();
+
+    res.json(leaderboard);
+
+});
+
+
+app.listen(3000,()=>{
+
+    console.log("Server running on port 3000");
 
 });
