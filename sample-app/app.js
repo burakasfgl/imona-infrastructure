cb54b97f0f1cd48bea6029c385944c3030b7815a
Burak Sofuoğlu <buraksm202@gmail.com>
2026-06-01 00:58:11 +0300

    mongo integration

--- sample-app/app.js
+++ sample-app/app.js
@@ -1,10 +1,40 @@
 const http = require("http");
+const { MongoClient } = require("mongodb");
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {  "Content-Type": "text/plain; charset=utf-8" });
-  res.end("CI CD ÇALIŞIYOR");
+const uri = process.env.MONGO_URI;
+
+async function connectMongo() {
+
+  try {
+
+    const client = new MongoClient(uri);
+
+    await client.connect();
+
+    console.log("Mongo Connected");
+
+  } catch (err) {
+
+    console.error(err);
+
+  }
+
+}
+
+connectMongo();
+
+const server = http.createServer((req,res)=>{
+
+  res.writeHead(200, {
+    "Content-Type":"text/plain; charset=utf-8"
+  });
+
+  res.end("Mongo Connected 🚀");
+
 });
 
-server.listen(3000, () => {
+server.listen(3000, ()=>{
+
   console.log("Server running on port 3000");
+
 });
